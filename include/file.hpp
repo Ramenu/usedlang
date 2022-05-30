d3d33ddfb20ec655c9cea023ad868ec3464ecfec
Ramenu <abamansoorr@gmail.com>
2022-05-30 17:06:35 -0400

    add getResultsWithListings fn

--- include/file.hpp
+++ include/file.hpp
@@ -2,8 +2,11 @@
 #define USEDLANG_FILE_HPP
 
 #include <vector>
-#include <string_view>
+#include <string>
 
-extern std::vector<std::pair<std::string_view, std::size_t>> getResults(const char *directoryPath);
+extern std::vector<std::pair<std::string, std::size_t>> getResults(const char *directoryPath, bool skipHidden);
+extern std::vector<std::pair<std::string, std::size_t>> getResultsWithListings(const char *directoryPath, 
+                                                                               const std::vector<std::string> &filesToList, 
+                                                                               bool skipHidden);
 
 #endif // USEDLANG_FILE_HPP
