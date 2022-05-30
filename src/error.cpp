4d1d47348a5ae6508d75447c6173b45413a906a6
Ramenu <abamansoorr@gmail.com>
2022-05-30 17:07:07 -0400

    Add function documentation

--- src/error.cpp
+++ src/error.cpp
@@ -2,6 +2,10 @@
 #include "color.h"
 #include <fmt/core.h>
 
+/**
+ * Prints an error message to stderr
+ * before terminating the program.
+ */
 void terminateWithError(const char *errorMessage) 
 {
     fmt::print(stderr, "{}FATAL ERROR:{} {}\n", COLOR_BOLDRED, COLOR_RESET, errorMessage);
