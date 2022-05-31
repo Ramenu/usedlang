980293b96027b83810e154c0eff02a433016163e
Ramenu <abamansoorr@gmail.com>
2022-05-30 20:17:11 -0400

    added abspath arg

--- include/file.hpp
+++ include/file.hpp
@@ -4,9 +4,10 @@
 #include <vector>
 #include <string>
 
-extern std::vector<std::pair<std::string, std::size_t>> getResults(const char *directoryPath, bool skipHidden);
+extern std::vector<std::pair<std::string, std::size_t>> getResults(const char *directoryPath, bool skipHidden, const std::string &executablePath);
 extern std::vector<std::pair<std::string, std::size_t>> getResultsWithListings(const char *directoryPath, 
                                                                                const std::vector<std::string> &filesToList, 
-                                                                               bool skipHidden);
+                                                                               bool skipHidden,
+                                                                               const std::string &executablePath);
 
 #endif // USEDLANG_FILE_HPP
