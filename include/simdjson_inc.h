f9f0ea542d34e37962ded43c576164700068608c
Ramenu <abamansoorr@gmail.com>
2022-06-01 23:05:07 -0400

    updated to include clang diagnostic removals

--- include/simdjson_inc.h
+++ include/simdjson_inc.h
@@ -1,13 +1,21 @@
 #ifndef SIMDJSON_INC_H
 #define SIMDJSON_INC_H
 
-#if defined(__GNUC__) || defined(__MINGW32__) || defined(__MINGW64__)
+#if (defined(__GNUC__) || defined(__MINGW32__) || defined(__MINGW64__)) && (!defined(__llvm__) && !defined(__INTEL_COMPILER))
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wuseless-cast"  
     #pragma GCC diagnostic ignored "-Wsuggest-override"  
     #pragma GCC diagnostic ignored "-Wextra-semi"
     #include "simdjson.h"
     #pragma GCC diagnostic pop
+#elif defined(__clang__)
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wpadded"
+    #pragma clang diagnostic ignored "-Wsign-conversion"
+    #pragma clang diagnostic ignored "-Wsuggest-override"
+    #pragma clang diagnostic ignored "-Wambiguous-reversed-operator"
+    #include "simdjson.h"
+    #pragma clang diagnostic pop
 #else
     #include "simdjson.h"
 #endif 
