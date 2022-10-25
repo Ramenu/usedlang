e8ee582a587eeb077eb19dc79c60e0e9a0110a7e
ramenu <ramenu101@gmail.com>
2022-10-25 18:34:40 -0400

    add noexcept specifiers to definitions

--- include/file.hpp
+++ include/file.hpp
@@ -4,10 +4,10 @@
 #include <vector>
 #include <string>
 
-extern std::vector<std::pair<std::string, std::size_t>> getResults(const char *directoryPath, bool skipHidden, const std::string &executablePath);
+extern std::vector<std::pair<std::string, std::size_t>> getResults(const char *directoryPath, bool skipHidden, const std::string &executablePath) noexcept;
 extern std::vector<std::pair<std::string, std::size_t>> getResultsWithListings(const char *directoryPath, 
                                                                                const std::vector<std::string> &filesToList, 
                                                                                bool skipHidden,
-                                                                               const std::string &executablePath);
+                                                                               const std::string &executablePath) noexcept;
 
 #endif // USEDLANG_FILE_HPP
